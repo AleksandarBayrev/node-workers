fcf31f442a699e6dd5e7e79ab3d947267536e6e6
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-03-28 13:47:14 +0200

    Refactor configuration

--- node-pg-main.ts
+++ node-pg-main.ts
@@ -1,18 +1,21 @@
 import { Worker } from "worker_threads";
+import fs from "fs/promises";
+import path from "path";
+const CONFIG_KEY = "NODE-PG-CONFIG";
+const configurationCache = new Map<string, object>();
+
+const getConfiguration = async () => {
+    if (!configurationCache.has(CONFIG_KEY)) {
+        configurationCache.set(CONFIG_KEY, JSON.parse((await fs.readFile(path.join(__dirname, "node-pg-config.json"))).toString()));
+    }
+    return configurationCache.get(CONFIG_KEY) as object;
+}
 
 const getClientData: () => Promise<string> = () => {
-    return new Promise((res, rej) => {
+    return new Promise(async (res, rej) => {
         const worker = new Worker("./node-pg-worker.js", {
             workerData: JSON.stringify({
-                clientConfig: {
-                    "host": "192.168.122.185",
-                    "user": "postgres",
-                    "password": "ws-2022-pgsql",
-                    "port": 5432,
-                    "database": "caches",
-                    "keepAlive": true,
-                    "application_name": "CacheSystem"
-                }
+                clientConfig: await getConfiguration()
             })
         });
         worker.on("message", (data) => res(data));
